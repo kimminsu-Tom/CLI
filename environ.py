e057f4e4ff3f83f057eac856168ca06527406dc3
kimminsu-Tom <rlaalstn06260@naver.com>
2023-06-08 00:43:24 +0900

    commit conflict solve

--- environ.py
+++ environ.py
@@ -6,13 +6,12 @@ def application(environ, start_response):
     ]
 
     response_body = '\n\n'.join(response_body)
-
     status = '200 OK'
     response_headers = [
         ('Content-Type', 'text/plain'),
         ('Content-Length', str(len(response_body)))
     ]
-    
+
     start_response(status, response_headers)
     return [response_body]
 
